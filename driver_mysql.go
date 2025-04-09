df8501b27e70b6bdac163f11bc75b712452a4b9f
Andrew Zhu <dir01@dir01.org>
2025-04-10 00:02:11 +0400

    refactor: Remove redundant MarkJobFailed and RescheduleJob methods from database drivers

--- driver_mysql.go
+++ driver_mysql.go
@@ -113,16 +113,6 @@ func (d *MySQLDriver) MarkJobProcessed(jobID int64, consumerName string) error {
 	return err
 }
 
-func (d *MySQLDriver) MarkJobFailed(jobID int64, errorMsg string) error {
-	_, err := d.db.Exec("UPDATE jobs SET retry_count = retry_count + 1, last_error = ? WHERE id = ?", errorMsg, jobID)
-	return err
-}
-
-func (d *MySQLDriver) RescheduleJob(jobID int64, scheduledAt time.Time) error {
-	_, err := d.db.Exec("UPDATE jobs SET scheduled_at = ? WHERE id = ?", scheduledAt, jobID)
-	return err
-}
-
 func (d *MySQLDriver) MarkJobFailedAndReschedule(jobID int64, errorMsg string, scheduledAt time.Time) error {
 	_, err := d.db.Exec(
 		"UPDATE jobs SET retry_count = retry_count + 1, last_error = ?, scheduled_at = ? WHERE id = ?",
--- driver_postgres.go
+++ driver_postgres.go
@@ -110,16 +110,6 @@ func (d *PostgresDriver) MarkJobProcessed(jobID int64, consumerName string) erro
 	return err
 }
 
-func (d *PostgresDriver) MarkJobFailed(jobID int64, errorMsg string) error {
-	_, err := d.db.Exec("UPDATE jobs SET retry_count = retry_count + 1, last_error = $1 WHERE id = $2", errorMsg, jobID)
-	return err
-}
-
-func (d *PostgresDriver) RescheduleJob(jobID int64, scheduledAt time.Time) error {
-	_, err := d.db.Exec("UPDATE jobs SET scheduled_at = $1 WHERE id = $2", scheduledAt, jobID)
-	return err
-}
-
 func (d *PostgresDriver) MarkJobFailedAndReschedule(jobID int64, errorMsg string, scheduledAt time.Time) error {
 	_, err := d.db.Exec(
 		"UPDATE jobs SET retry_count = retry_count + 1, last_error = $1, scheduled_at = $2 WHERE id = $3",
